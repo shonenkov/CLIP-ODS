72e57f4b905c4178ce3241e4d05bc12ae5625ae9
shonenkov <shonenkov@phystech.edu>
2021-04-09 15:39:22 +0300

    initial version

---
+++ clip_ods/detector.py
@@ -0,0 +1,16 @@
+from ensemble_boxes import weighted_boxes_fusion
+
+
+class CLIPDetectorV0:
+
+    def __init__(self):
+        """
+        First version, required improving of quality and speed :)
+        """
+        pass
+
+    def detect_by_text(self, search_text):
+        pass
+
+    def detect_coco(self):
+        pass
