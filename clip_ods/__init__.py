2bcadc5327f580fca2f839f5b7df0bc3a83fd0f2
shonenkov <shonenkov@phystech.edu>
2021-04-16 12:09:21 +0300

    change version

--- clip_ods/__init__.py
+++ clip_ods/__init__.py
@@ -1,7 +1,7 @@
 from . import clip, utils
-from .detector import CLIPDetectorV0
+from .detector import CLIPDetectorV0, CLIPDetectorV1
 
-__all__ = ['clip', 'utils', 'CLIPDetectorV0']
+__all__ = ['clip', 'utils', 'CLIPDetectorV0', 'CLIPDetectorV1']
 
-__version__ = '0.0.1-rc1'
+__version__ = '0.0.1-rc2'
 
